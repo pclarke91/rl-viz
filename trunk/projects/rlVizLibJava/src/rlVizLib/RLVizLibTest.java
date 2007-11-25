2049c9d51625c21e9dd8e51ea35b559e1a9d96d7
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2007-11-25 23:27:19 +0000

    Up to date version of RLVizLib

---
+++ trunk/projects/rlVizLibJava/src/rlVizLib/RLVizLibTest.java
@@ -0,0 +1,10 @@
+package RLVizLib;
+public class RLVizLibTest extends junit.framework.TestCase {
+
+    public void testNothing() {
+    }
+    
+    public void testWillAlwaysFail() {
+        fail("An error message");
+    }
+}
