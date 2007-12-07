aba97cb64723006eea3b4a0f083fba67ba2eba86
brian <brian@tannerpages.com>
2007-12-07 01:16:35 +0000

    played with tests

---
+++ projects/rlVizLibJava/tests/rlVizLib/messaging/environment/EpisodeSummaryRequestTest.java
@@ -0,0 +1,11 @@
+package rlVizLib.messaging.environment;
+
+import rlVizLib.messaging.environment.EpisodeSummaryRequest;
+public class EpisodeSummaryRequestTest extends junit.framework.TestCase {
+
+	public void testNothing() {
+    }
+    
+	public void testWillAlwaysFail() {
+    }
+}
