93e3362de668382b94408e05b05f8fd3d8779066
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2007-12-07 02:55:41 +0000

    added rlVizCore class that gets version

---
+++ trunk/projects/rlVizLibJava/src/rlVizLib/rlVizCore.java
@@ -0,0 +1,11 @@
+package rlVizLib;
+
+public final class rlVizCore {
+private static  final int versionMajor=1;
+static final int versionMinor=1;
+
+public static final int getVersionMajor(){return versionMajor;}
+public static final int getVersionMinor(){return versionMinor;}
+
+
+}
