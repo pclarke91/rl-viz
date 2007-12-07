433e412fdba9b8d66c4d436f6e7877d61fe0cf8a
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2007-12-07 00:51:50 +0000

    updating bit manipulation stuff to be in Utilityshop

--- trunk/projects/rlVizLibJava/src/rlVizLib/utilities/UtilityShop.java
+++ trunk/projects/rlVizLibJava/src/rlVizLib/utilities/UtilityShop.java
@@ -130,4 +130,57 @@ public class UtilityShop {
            P.addStringParam("###description",provider.getDescription());
        }
    }
+
+	public static int putSomeBitsFromIntIntoInt(int A, int B, int amount, int offset) throws Exception {
+		if (A < 0)
+			throw new Exception();
+		if (B < 0)
+			throw new Exception();
+		if (amount < 0)
+			throw new Exception();
+		if (offset < 0)
+			throw new Exception();
+		if (amount + offset >= 32)
+			throw new Exception();
+
+		if (amount == 0)
+			return B;
+		// mask off higher values from A
+		int mask = (1 << amount) - 1;
+		A = A & mask;
+		// shift A up to offset
+		A = A << offset;
+		mask = mask << offset;
+		// zero out this section in B
+		B = B & (~mask);
+		// bitwise or shifted A with cleared B
+		B = B | A;
+		return B;
+	}
+	
+	public static int extractSomeBitsAsIntFromInt(int A, int B, int amount, int offset) throws Exception {
+		if (A < 0)
+			throw new Exception();
+		if (B < 0)
+			throw new Exception();
+		if (amount < 0)
+			throw new Exception();
+		if (offset < 0)
+			throw new Exception();
+		if (amount + offset >= 32)
+			throw new Exception();
+
+		if (amount == 0)
+			return A;
+		// shift down B
+		B = B >> offset;
+		// mask off higher values from B
+		int mask = (1 << amount) - 1;
+		B = B & mask;
+		// zero out section in A
+		A = A & (~mask);
+		// or B with A
+		A = A | B;
+		return A;
+	}
 }
