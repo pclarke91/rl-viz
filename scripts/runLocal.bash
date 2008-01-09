75060cc0561e3c9f14580c03e7080db1ffee3de1
radkie <radkie@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2008-01-09 22:39:31 +0000

    fixed the runLocal script

--- scripts/runLocal.bash
+++ scripts/runLocal.bash
@@ -1,2 +1,2 @@
 #/bin/bash
-java -Xmx1024M  -DRLVIZ_LIB_PATH=../../rlcomplibrary/trunk/libraries/ -classpath ../system/dist/RLVizApp.jar:../system/dist/RLVizLib.jar:../system/dist/EnvironmentShell.jar:../system/dist/AgentShell.jar -DCPPEnv=true btViz.LocalGraphicalDriver
+java -Xmx1024M  -DRLVIZ_LIB_PATH=../../rlcomplibrary/libraries -classpath ../system/dist/RLVizApp.jar:../system/dist/RLVizLib.jar:../system/dist/EnvironmentShell.jar:../system/dist/AgentShell.jar -DCPPEnv=true btViz.LocalGraphicalDriver
