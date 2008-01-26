758dc3c60d22418f20d3d64d4a3221290d51c06f
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2008-01-26 08:22:38 +0000

    removing the stable tag

--- tags/versions/stable/scripts/runLocal.bash
+++
@@ -1,2 +0,0 @@
-#/bin/bash
-java -Xmx1024M  -DRLVIZ_LIB_PATH=../../rl-library/system/dist -classpath ../system/dist/RLVizApp.jar:../system/dist/RLVizLib.jar:../system/dist/EnvironmentShell.jar:../system/dist/AgentShell.jar btViz.LocalGraphicalDriver
