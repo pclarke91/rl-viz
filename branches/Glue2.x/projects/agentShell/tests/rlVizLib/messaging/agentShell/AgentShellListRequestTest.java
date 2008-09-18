a78bfc4dc177fedb6b8f20fe624717946a4cfdb9
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2008-09-18 17:27:26 +0000

    Creating a branch for rl-glue 2.x compatibility for those who might want it

---
+++ branches/Glue2.x/projects/agentShell/tests/rlVizLib/messaging/agentShell/AgentShellListRequestTest.java
@@ -0,0 +1,47 @@
+/*
+Copyright 2007 Brian Tanner
+brian@tannerpages.com
+http://brian.tannerpages.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package rlVizLib.messaging.agentShell;
+
+import agentShell.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author btanner
+ */
+public class AgentShellListRequestTest {
+
+    public AgentShellListRequestTest() {
+    }
+
+    @Test
+    public void TestEnvList(){
+    System.setProperty("RLVIZ_LIB_PATH", "/Users/btanner/Documents/Java-Projects/rl-library/system/dist");
+    AgentShell AS=new AgentShell();
+    String theQuery=rlVizLib.messaging.agentShell.AgentShellListRequest.makeRequest();
+        
+    String firstResponse=AS.agent_message(theQuery);
+    String secondResponse=AS.agent_message(theQuery);
+        
+    assertTrue(firstResponse.compareTo(secondResponse)==0);
+    }
+
+}
