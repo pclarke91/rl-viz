dd9969c05146dfa28db7a6ef0d8d1524977244d7
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2008-11-12 05:38:39 +0000

    Added disable method to value function which will make it not draw.

--- trunk/projects/rlVizLibJava/src/rlVizLib/visualization/AgentOnValueFunctionVizComponent.java
+++ trunk/projects/rlVizLibJava/src/rlVizLib/visualization/AgentOnValueFunctionVizComponent.java
@@ -3,21 +3,20 @@ Copyright 2007 Brian Tanner
 brian@tannerpages.com
 http://brian.tannerpages.com
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-  
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package rlVizLib.visualization;
+
 import java.util.Observable;
 import rlVizLib.visualization.interfaces.AgentOnValueFunctionDataProvider;
 import rlVizLib.utilities.UtilityShop;
@@ -29,40 +28,66 @@ import java.util.Observer;
 import rlVizLib.general.TinyGlue;
 import rlVizLib.visualization.interfaces.GlueStateProvider;
 
-
 public class AgentOnValueFunctionVizComponent implements SelfUpdatingVizComponent, Observer {
-        private VizComponentChangeListener theChangeListener;
-        private AgentOnValueFunctionDataProvider dataProvider;
-        
-	public AgentOnValueFunctionVizComponent(AgentOnValueFunctionDataProvider dataProvider,TinyGlue theGlueState){
-		this.dataProvider=dataProvider;
-                theGlueState.addObserver(this);
-	}
 
-	public void render(Graphics2D g) {
-		dataProvider.updateAgentState();
-		g.setColor(Color.BLUE);
+    private VizComponentChangeListener theChangeListener;
+    private AgentOnValueFunctionDataProvider dataProvider;
+    private boolean enabled = true;
 
-		double transX=UtilityShop.normalizeValue( dataProvider.getCurrentStateInDimension(0),
-				dataProvider.getMinValueForDim(0),
-				dataProvider.getMaxValueForDim(0));
-
-		double transY=UtilityShop.normalizeValue( dataProvider.getCurrentStateInDimension(1),
-				dataProvider.getMinValueForDim(1),
-				dataProvider.getMaxValueForDim(1));
-
-
-		Rectangle2D agentRect=new Rectangle2D.Double(transX,transY,.02,.02);
-		g.fill(agentRect);
-	}
-    public void setVizComponentChangeListener(VizComponentChangeListener theChangeListener) {
-        this.theChangeListener=theChangeListener;
+    public AgentOnValueFunctionVizComponent(AgentOnValueFunctionDataProvider dataProvider, TinyGlue theGlueState) {
+        this.dataProvider = dataProvider;
+        theGlueState.addObserver(this);
     }
 
-    public void update(Observable o, Object arg) {
-        if(theChangeListener!=null){
-        theChangeListener.vizComponentChanged(this);
+    public void setEnabled(boolean newEnableValue) {
+        if (newEnableValue == false && this.enabled) {
+            disable();
+        }
+        if (newEnableValue == true && !this.enabled) {
+            enable();
         }
     }
 
+    private void disable() {
+        enabled = false;
+        theChangeListener.vizComponentChanged(this);
+    }
+
+    private void enable() {
+        enabled = true;
+    }
+
+    public void render(Graphics2D g) {
+        if (!enabled) {
+            Color myClearColor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+            g.setColor(myClearColor);
+            g.setBackground(myClearColor);
+            g.clearRect(0, 0, 1, 1);
+            return;
+        }
+        dataProvider.updateAgentState();
+        g.setColor(Color.BLUE);
+
+        double transX = UtilityShop.normalizeValue(dataProvider.getCurrentStateInDimension(0),
+                dataProvider.getMinValueForDim(0),
+                dataProvider.getMaxValueForDim(0));
+
+        double transY = UtilityShop.normalizeValue(dataProvider.getCurrentStateInDimension(1),
+                dataProvider.getMinValueForDim(1),
+                dataProvider.getMaxValueForDim(1));
+
+
+        Rectangle2D agentRect = new Rectangle2D.Double(transX, transY, .02, .02);
+        g.fill(agentRect);
+    }
+
+    public void setVizComponentChangeListener(VizComponentChangeListener theChangeListener) {
+        this.theChangeListener = theChangeListener;
+    }
+
+    public void update(Observable o, Object arg) {
+        if (theChangeListener != null) {
+            theChangeListener.vizComponentChanged(this);
+        }
+    }
 }
