acd8a7960bc6395aca733709894a9318fef30769
brian@tannerpages.com <brian@tannerpages.com@d4e37871-b43f-0410-b638-613b0fcc8c0d>
2009-10-16 23:43:20 +0000

    Fixed a bug so that RLVizApp would not crash if list-agents and list-envs were both set to false.  Also added some parameters to set default speed, autoload, etc.

--- trunk/projects/rlVizApp/src/org/rlcommunity/rlviz/app/frames/RLVizFrame.java
+++ trunk/projects/rlVizApp/src/org/rlcommunity/rlviz/app/frames/RLVizFrame.java
@@ -3,20 +3,18 @@ Copyright 2007 Brian Tanner
 brian@tannerpages.com
 http://brian.tannerpages.com
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package org.rlcommunity.rlviz.app.frames;
 
 import java.awt.Color;
@@ -25,31 +23,29 @@ import org.rlcommunity.rlviz.app.RLControlPanel;
 import org.rlcommunity.rlviz.app.RLGlueLogic;
 import java.awt.Dimension;
 import javax.swing.BoxLayout;
-import javax.swing.JPanel;
+import org.rlcommunity.rlviz.settings.RLVizSettings;
 
-public class RLVizFrame extends GenericVizFrame{
+public class RLVizFrame extends GenericVizFrame {
 
     //Components
-
     VisualizerPanel ePanel = null;
     VisualizerPanel aPanel = null;
     RLGlueLogic theGlueConnection = null;
     static String programName = "RLVizApp";
     private static final long serialVersionUID = 1L;
-    
     private boolean useEnvVisualizer = false;
     private boolean useAgentVisualizer = false;
 
-    public RLVizFrame(){
+    public RLVizFrame() {
         this(true, false);
     }
-    
+
     public RLVizFrame(boolean useEnvVisualizer, boolean useAgentVisualizer) {
         super();
 
-        this.useEnvVisualizer=useEnvVisualizer;
-        this.useAgentVisualizer=useAgentVisualizer;
-        
+        this.useEnvVisualizer = useEnvVisualizer;
+        this.useAgentVisualizer = useAgentVisualizer;
+
 
 
         theGlueConnection = RLGlueLogic.getGlobalGlueLogic();
@@ -64,43 +60,45 @@ public class RLVizFrame extends GenericVizFrame{
             panelHeight /= 2;
         }
 
-        JPanel controlPanel = new RLControlPanel(theGlueConnection);
+        final RLControlPanel controlPanel = new RLControlPanel(theGlueConnection);
         getContentPane().add(controlPanel);
 
         if (useEnvVisualizer) {
-            envVizFrame=new EnvVisualizerFrame(new Dimension(panelWidth,panelHeight));
+            envVizFrame = new EnvVisualizerFrame(new Dimension(panelWidth, panelHeight));
             envVizFrame.setLocation(10, 10);
         }
 
         if (useAgentVisualizer) {
-            agentVizFrame=new AgentVisualizerFrame(new Dimension(panelWidth,panelHeight));
+            agentVizFrame = new AgentVisualizerFrame(new Dimension(panelWidth, panelHeight));
             agentVizFrame.setLocation(10, 10);
 
 
-            if(!useEnvVisualizer)
-            agentVizFrame.setLocation(10, 10);
-            else
-            agentVizFrame.setLocation(10,panelHeight+30);
+            if (!useEnvVisualizer) {
+                agentVizFrame.setLocation(10, 10);
+            } else {
+                agentVizFrame.setLocation(10, panelHeight + 30);
+            }
         }
 
         setFrames(this, envVizFrame, agentVizFrame);
         makeMenus();
 
-        if(envVizFrame!=null){
+        if (envVizFrame != null) {
             envVizFrame.setFrames(this, envVizFrame, agentVizFrame);
             envVizFrame.makeMenus();
             envVizFrame.setVisible(true);
         }
-        if(agentVizFrame!=null){
+        if (agentVizFrame != null) {
             agentVizFrame.setFrames(this, envVizFrame, agentVizFrame);
             agentVizFrame.makeMenus();
             agentVizFrame.setVisible(true);
         }
-        
-        if(!useAgentVisualizer&&!useEnvVisualizer)
-            setLocation(10,10);
-        else
-            setLocation(panelWidth+20,10);
+
+        if (!useAgentVisualizer && !useEnvVisualizer) {
+            setLocation(10, 10);
+        } else {
+            setLocation(panelWidth + 20, 10);
+        }
 
 
 
@@ -113,7 +111,14 @@ public class RLVizFrame extends GenericVizFrame{
         //Make sure we exit if they close the window
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle(programName);
+
+        
+        if (RLVizSettings.getBooleanSetting("autoload")) {
+            new Thread(new Runnable() {
+                public void run() {
+                    controlPanel.doLoad();
+                }
+            }).start();
+        }
     }
-
-
 }
